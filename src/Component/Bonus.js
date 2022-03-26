043083b342ad72e50e368e0ca51a511eb02e7dc1
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-03-26 23:46:58 +0600

    first commit

--- src/Component/Bonus.js
+++ src/Component/Bonus.js
@@ -7,31 +7,37 @@ function Bonus() {
   <div className="accordion-item">
     <h2 className="accordion-header" id="headingOne">
       <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-      <h5>How React Work</h5>
+      <h5> <strong>How React Work?</strong></h5>
       </button>
     </h2>
     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div className="accordion-body">
-      The main concept of React is component. A React component can be composed of React components and ReactElement. Following the React way, you divide your app into several components according to function and responsibility.
-        </div>
+      <p>It's a library, after all. To create user interfaces. This is ideal because, most of the time, this is all we desire. The most appealing aspect of this description is how it emphasizes React's simplicity. It isn't a massive framework. It isn't a full-stack solution capable of handling everything from the database to real-time changes via web socket connections. Most of these pre-packaged solutions aren't what we want because, in the end, they frequently create more problems than they solve. Facebook paid attention to what we wanted.</p>
+      </div>
     </div>
   </div>
   <div className="accordion-item">
     <h2 className="accordion-header" id="headingTwo">
       <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-      <h5>How useState Works</h5>
+      <h5><strong> How useState() Works?</strong></h5>
       </button>
     </h2>
     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div className="accordion-body">
-      The useState() is a Hook that allows you to have state variables in functional components. The useState hook is a special function that takes the initial state as an argument and returns an array of two entries
+      <p>The useState() is a built-in React Hook that allows you for having state variables in 
+        functional components. It should be used when the DOM has something that is 
+        dynamically manipulating/controlling. We initialize our state by calling useState in 
+        our function component.
+        useState accepts an initial state and returns two values:
+        <li>The current state.</li>
+        <li>A function that updates the state.</li></p>
          </div>
     </div>
   </div>
   <div className="accordion-item">
     <h2 className="accordion-header" id="headingThree">
       <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        <h5>Props VS State</h5>
+        <h5> <strong>Props VS State?</strong></h5>
       </button>
     </h2>
     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
